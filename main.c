d56323ccd954ba8039d0267717c2fef396f712de
clyde-tressler <amalgamtedclyde@gmail.com>
2022-12-14 13:38:20 -0500

    updating main

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "tiffio.h"
+#include "tiff.h"
 
 
 int main(int argc, char* argv[])
-{   int rows = atoi(argv[1]);
+
+{   uint32_t rows = atoi(argv[1]);
     int cols = atoi(argv[2]);
-    u_int8_t imageData[rows][cols];
+    u_int16_t imageData[rows][cols];
     for (int i=0; i<rows; i++){
         for (int j=0; j<cols; j++){
-            imageData[i][j] = 255;
+            imageData[i][j] = 65535;
         }
     }
     for (int i=0; i<rows; i++){
         imageData[i][i] = 0;
     }
-
+    TIFF* tif = TIFFOpen("test.tif", "w");
+    tdata_t buf;
+    for (int row=0; row<rows; rows++) {
+        TIFFWriteScanline(tif, buf, row, 65535);
+    }
+    TIFFClose(tif);
 }
